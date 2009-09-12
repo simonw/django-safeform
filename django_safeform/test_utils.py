bfeca350a15172c19775cea7acbf55234c034893
Simon Willison <simon@simonwillison.net>
2009-09-12 23:31:49 +0100

    code cleanup

--- django_safeform/test_utils.py
+++ django_safeform/test_utils.py
@@ -1,9 +1,16 @@
+"""
+Simple functions for extracting input tags from HTML - when you're testing 
+CSRF protection you need to be able to pull out the
+    <input type="hidden" name="_csrf_token" value="...">
+tags so you can POST a correct value.
+"""
 import re
 
 input_re = re.compile('<input(.*?)>')
 attrib_re = re.compile(r'(\w+)=(["\'])(.*?)(\2)')
 
 def extract_input_tag_attrs(html):
+    "Returns a list of HTML attribute dictionaries for all inputs found"
     input_tags = []
     for input in input_re.findall(html):
         pairs = []
@@ -13,6 +20,7 @@ def extract_input_tag_attrs(html):
     return input_tags
 
 def extract_input_tags(html):
+    "Returns a dictionary of name => value for all inputs found"
     tag_attrs = extract_input_tag_attrs(html)
     return dict([
         (d['name'], d.get('value', ''))
